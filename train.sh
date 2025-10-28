089e40209f176362c5ae62ad50cdcdaef3f7101b
Jacopo Minniti <jacopo.minniti004@gmail.com>
2025-10-28 10:29:15 -0400

    attempt to greedy

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=evojax
-#SBATCH --time=04:00:00
-#SBATCH --gpus-per-node=h100:1
+#SBATCH --time=01:00:00
+#SBATCH --gpus-per-node=l40s:4
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=8G
+#SBATCH --mem=32G
 #SBATCH --output=/scratch/jacopo04/evojax/jobs_logs/evojax-%j.out
 #SBATCH --error=/scratch/jacopo04/evojax/jobs_logs/evojax-%j.err
 #SBATCH -D /scratch/jacopo04/evojax
